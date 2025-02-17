93bed13f2b6c04275e665fc7e57fbd3a3932bc06
ferreirasegundo <segundo.ferreira28@gmail.com>
2025-02-17 09:43:33 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateExcuse ();
 };
+function generateExcuse() {
+  let who = ["Mi amigo", "La novia", "El hermano", "La mama"];
+  let action = ["se durmio", "no lo deja salir", "destrozó", "rompió"];
+  let what = ["mi zapato", "mi teléfono", "el coche"];
+  let when = ["antes de salir", "mientras dormía", "cuando estaba listo", "durante el almuerzo"];
+
+  let excuse =
+  who[Math.floor(Math.random() * who.length)] +
+  " " +
+  action[Math.floor(Math.random() * action.length)] +
+  " " +
+  what[Math.floor(Math.random() * what.length)] +
+  " " +
+  when[Math.floor(Math.random() * when.length)] +
+  ".";
+
+  document.getElementById("excuse").innerHTML = excuse;
+}
